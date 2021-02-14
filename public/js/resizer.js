b59fae8143a46d43974798f61d02fb781f6b1e82
michael-andreuzza <michaelandreuzzavega@gmail.com>
2021-02-14 14:08:00 +0200

    clients and pricing ready with new layout and modified markup too

--- public/js/resizer.js
+++
@@ -1,9 +0,0 @@
-$(".panel-left").resizable({
-   handleSelector: ".splitter",
-   resizeHeight: false
- });
-
- $(".panel-top").resizable({
-   handleSelector: ".splitter-horizontal",
-   resizeWidth: false
- });
