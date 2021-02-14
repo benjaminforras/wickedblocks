a57f60219112677c597c434b7d70a12f1443ae0d
michael-andreuzza <michaelandreuzzavega@gmail.com>
2021-02-14 18:26:30 +0200

    nav and footers ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,19 @@ module.exports = {
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
+      zIndex: {
+        '0': 0,
+       '10': 10,
+       '20': 20,
+       '30': 30,
+       '40': 40,
+       '50': 50,
+       '25': 25,
+       '50': 50,
+       '75': 75,
+       '100': 100,
+        'auto': 'auto',
+      },
 
       boxShadow: {
         smallest: '0px 4px 8px rgba(248, 250, 252, 0.12)',
